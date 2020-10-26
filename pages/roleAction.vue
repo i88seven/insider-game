6a82892eb6df98359f81ab2b7e8e00733f0c6fb3
i88seven <i88.jelounjem@gmail.com>
2020-10-27 05:03:38 +0900

    Add roleAction page

---
+++ pages/roleAction.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      あなたはマスターです。
+    </v-card-title>
+    <v-card-text>
+      <v-text-field
+        v-model="subject"
+        :rules="[rules.required, rules.counter]"
+        label="お題"
+        placeholder="お題を決めてください"
+        counter
+        maxlength="20"
+      ></v-text-field>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        color="primary"
+        nuxt
+        to="/inspire"
+      >
+        決定
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data () {
+    return {
+      subject: '',
+      rules: {
+        required: value => !!value || '入力してください',
+        counter: value => value.length <= 20 || '20文字以下にしてください。',
+      },
+    }
+  },
+}
+</script>
