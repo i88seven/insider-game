9165ada99056854f681d94342f3a556991a07e99
i88seven <i88.jelounjem@gmail.com>
2021-01-01 23:04:06 +0900

    Add debug message

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <v-card>
     <v-card-title class="headline">インサイダーゲーム</v-card-title>
     <v-card-text>LINE にログインします</v-card-text>
+    <v-card-text>{{ debugMsg }}</v-card-text>
   </v-card>
 </template>
 
@@ -13,17 +14,30 @@ import { gameContentStore } from '~/store';
 export default Vue.extend({
   name: 'Root',
 
+  data(): { debugMsg: string } {
+    return {
+      debugMsg: '',
+    };
+  },
   async mounted(): Promise<void> {
+    this.debugMsg = 'before init store';
     gameContentStore.init();
+    this.debugMsg = 'after init store';
     const roomId = this.$route.query.roomId ? this.$route.query.roomId.toString() : '';
+    this.debugMsg = `roomId: ${roomId}`;
     await gameContentStore.initLiff();
+    this.debugMsg = `after init liff`;
     if (!liff.isLoggedIn()) {
+      this.debugMsg = `is logged in`;
       await gameContentStore.loginLiff();
+      this.debugMsg = `after login liff`;
       return;
     }
     if (roomId) {
+      this.debugMsg = `roomId: ${roomId}`;
       this.$router.push({ path: 'main', query: { roomId } });
     } else {
+      this.debugMsg = `no roomId`;
       this.$router.push('main');
     }
   },
