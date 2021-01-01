055750d50ceb77f953488fcd2a137f52bcf566ac
i88seven <i88.jelounjem@gmail.com>
2021-01-02 01:41:38 +0900

    Remove debug code

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,6 @@
   <v-card>
     <v-card-title class="headline">インサイダーゲーム</v-card-title>
     <v-card-text>LINE にログインします</v-card-text>
-    <v-card-text>{{ debugMsg }}</v-card-text>
   </v-card>
 </template>
 
@@ -14,38 +13,22 @@ import { gameContentStore } from '~/store';
 export default Vue.extend({
   name: 'Root',
 
-  data(): { debugMsg: string } {
-    return {
-      debugMsg: '',
-    };
-  },
   async mounted(): Promise<void> {
-    this.debugMsg = 'before init store';
     gameContentStore.init();
-    this.debugMsg = 'after init store';
     const roomId = this.$route.query.roomId ? this.$route.query.roomId.toString() : '';
-    this.debugMsg = `roomId: ${roomId}, liffId: ${process.env.LIFF_ID}`;
     liff
       .init({ liffId: process.env.LIFF_ID || '' })
       .then(() => {
         if (!liff.isInClient() && !liff.isLoggedIn()) {
           liff.login({ redirectUri: location.href });
         }
-        this.debugMsg = `after init liff`;
       })
       .catch((err) => {
-        this.debugMsg = err;
+        console.log('LIFF Initialization failed.', err);
       });
-    // if (!liff.isLoggedIn()) {
-    //   this.debugMsg = `is logged in`;
-    //   this.debugMsg = `after login liff`;
-    //   return;
-    // }
     if (roomId) {
-      this.debugMsg = `roomId: ${roomId}`;
       this.$router.push({ path: 'main', query: { roomId } });
     } else {
-      this.debugMsg = `no roomId`;
       this.$router.push('main');
     }
   },
